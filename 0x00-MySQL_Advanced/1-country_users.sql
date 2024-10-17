51bdcaef5a9246044b38a54fcefe1e42979a00ff
lamecksingu <singulameck9619@gmail.com>
2024-10-17 15:23:45 +0300

    In and Out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Create users table
+CREATE TABLE IF NOT EXISTS users (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US'
+);
