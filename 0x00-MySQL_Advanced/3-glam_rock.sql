c7b07429f2602ad8fc6b686fc9e22b0679170a55
lamecksingu <singulameck9619@gmail.com>
2024-10-17 15:27:08 +0300

    Old school band

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,13 @@
+-- Lists all bands with 'Glam rock' as their main style,
+-- ranked by their longevity
+SELECT
+	band_name,
+	(
+		CASE
+		WHEN split IS NULL THEN 2022
+		ELSE split
+		END - formed
+	) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
