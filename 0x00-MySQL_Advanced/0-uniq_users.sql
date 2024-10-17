af2342ba709dd2a6bbcc92a5b9a77a3a4eb3a904
lamecksingu <singulameck9619@gmail.com>
2024-10-17 15:21:31 +0300

    We are all unique!

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Create users table
+CREATE TABLE IF NOT EXISTS users (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
---
+++ 0x00-MySQL_Advanced/README.md
@@ -0,0 +1,1 @@
+ADvanced techniques in MySQL
